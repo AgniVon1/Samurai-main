433566aeaaae8ef22d8d54d1053b11c4e1e68028
AgniVon1 <iliatron00@gmail.com>
2023-03-05 21:48:24 +0300

    fix:instans setting

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,23 +3,23 @@ import axios from "axios";
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': 'a975d2eb-6b11-4e02-9b1f-9d435ea8aed2'
+        'API-KEY': '0a13c5b1-a427-400f-be11-b50e68ff8029'
     }
 }
 const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     ...settings
 })
 
 export const API = {
     getUsers(currentPage = 1,pageSize = 10) {
-        return instance.get(`/users?page=${currentPage}&count=${pageSize}`).then(res => (res.data));
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(res => (res.data));
     },
     follow(id:string){
-        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`,{}).then(res => (res.data))
+        return instance.post(`follow/${id}`,{}).then(res => (res.data))
     },
     unFollow(id:string){
-        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`).then(res => (res.data))
+        return instance.delete(`follow/${id}`).then(res => (res.data))
     }
 }
 
